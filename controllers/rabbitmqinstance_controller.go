a2ccba50f4ddab849c447e67e12711fdf57d6d64
Gabriele Santomaggio <g.santomaggio@gmail.com>
2019-11-15 12:18:12 +0100

    add

--- controllers/rabbitmqinstance_controller.go
+++ controllers/rabbitmqinstance_controller.go
@@ -45,8 +45,9 @@ type RabbitMQInstanceReconciler struct {
 }
 
 var (
-	ownerKey = ".metadata.controller"
-	apiGVStr = susev1beta1.GroupVersion.String()
+	ownerKey                      = ".metadata.controller"
+	apiGVStr                      = susev1beta1.GroupVersion.String()
+	terminationGracePeriodSeconds = int64(10)
 )
 
 // +kubebuilder:rbac:groups=suse.suse.rabbitmq-operator,resources=rabbitmqinstances,verbs=get;list;watch;create;update;patch;delete
@@ -62,16 +63,16 @@ func (r *RabbitMQInstanceReconciler) Reconcile(req ctrl.Request) (ctrl.Result, e
 		return ctrl.Result{}, err
 	}
 	log.Printf("Checking status of res: %d req: %s", instance.Spec.Replicas, req.NamespacedName.Name)
-	deployment := &appsv1.Deployment{}
-	err = r.Get(context.TODO(), types.NamespacedName{Name: instance.Name, Namespace: instance.Namespace}, deployment)
+	statefulset := &appsv1.StatefulSet{}
+	err = r.Get(context.TODO(), types.NamespacedName{Name: instance.Name, Namespace: instance.Namespace}, statefulset)
 
 	if err != nil && errors.IsNotFound(err) {
 		// Define a new Deployment
-		dep := r.deploymentForRabbitMQ(instance)
-		log1.Info("Creating a new Deployment.", "Deployment.Namespace", dep.Namespace, "Deployment.Name", dep.Name)
+		dep, err := r.statefulsetForRabbitMQ(instance)
+		log1.Info("Creating a new statefulset.", "statefulset.Namespace", dep.Namespace, "statefulset.Name", dep.Name)
 		err = r.Create(context.TODO(), dep)
 		if err != nil {
-			log1.Error(err, "Failed to create new Deployment.", "Deployment.Namespace", dep.Namespace, "Deployment.Name", dep.Name)
+			log1.Error(err, "Failed to create new statefulset.", "statefulset.Namespace", dep.Namespace, "statefulset.Name", dep.Name)
 			return reconcile.Result{}, err
 		}
 		// Deployment created successfully - return and requeue
@@ -81,6 +82,33 @@ func (r *RabbitMQInstanceReconciler) Reconcile(req ctrl.Request) (ctrl.Result, e
 	} else if err != nil {
 		log1.Error(err, "Failed to get Deployment.")
 		return reconcile.Result{}, err
+	}
+
+	service := &corev1.Service{}
+	err = r.Get(context.TODO(), types.NamespacedName{Name: instance.Name, Namespace: instance.Namespace}, service)
+	if err != nil && errors.IsNotFound(err) {
+		// Define a new Service object
+		ser := r.serviceForRabbitMQ(instance)
+		log1.Info("Creating a new Service.", "Service.Namespace", ser.Namespace, "Service.Name", ser.Name)
+		err = r.Create(context.TODO(), ser)
+		if err != nil {
+			log1.Error(err, "Failed to create new Service.", "Service.Namespace", ser.Namespace, "Service.Name", ser.Name)
+			return reconcile.Result{}, err
+		}
+	} else if err != nil {
+		log1.Error(err, "Failed to get Service.")
+		return reconcile.Result{}, err
+	}
+
+	// Ensure the StatefulSet size is the same as the spec
+	size := instance.Spec.Replicas
+	if *statefulset.Spec.Replicas != size {
+		statefulset.Spec.Replicas = &size
+		err = r.Update(context.TODO(), statefulset)
+		if err != nil {
+			log1.Error(err, "Failed to update StatefulSet.", "StatefulSet.Namespace", instance.Namespace, "StatefulSet.Name", instance.Name)
+			return reconcile.Result{}, err
+		}
 	}
 
 	return ctrl.Result{}, nil
@@ -153,42 +181,68 @@ func labelsForRabbitMQ(name string) map[string]string {
 	return map[string]string{"app": "rabbitmq", "rabbitmq_cr": name}
 }
 
-// deploymentForRabbitMQ returns a rabbitmq Deployment object
-func (r *RabbitMQInstanceReconciler) deploymentForRabbitMQ(m *susev1beta1.RabbitMQInstance) *appsv1.Deployment {
-	ls := labelsForRabbitMQ(m.Name)
-	instance := &susev1beta1.RabbitMQInstance{}
+func labelSelector(labels map[string]string) *metav1.LabelSelector {
+	return &metav1.LabelSelector{MatchLabels: labels}
+}
 
-	replicas := instance.Spec.Replicas
-
-	dep := &appsv1.Deployment{
+// statefulsetForRabbitMQ returns a rabbitmq statefulset object
+func (r *RabbitMQInstanceReconciler) statefulsetForRabbitMQ(m *susev1beta1.RabbitMQInstance) (*appsv1.StatefulSet, error) {
+	labels := labelsForRabbitMQ(m.Name)
+	replicas := m.Spec.Replicas
+	statefulset := &appsv1.StatefulSet{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "StatefulSet",
+			APIVersion: "apps/v1",
+		},
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      m.Name,
 			Namespace: m.Namespace,
+			Labels:    labels,
 		},
-		Spec: appsv1.DeploymentSpec{
-			Replicas: &replicas,
-			Selector: &metav1.LabelSelector{
-				MatchLabels: ls,
-			},
+		Spec: appsv1.StatefulSetSpec{
+			Selector:    labelSelector(labels),
+			ServiceName: m.Name,
+			Replicas:    &replicas,
 			Template: corev1.PodTemplateSpec{
 				ObjectMeta: metav1.ObjectMeta{
-					Labels: ls,
+					Labels: labels,
 				},
 				Spec: corev1.PodSpec{
-					Containers: []corev1.Container{{
-						Image: "rabbitmq:3.8-management",
-						Name:  "rabbitmq",
-						//						Command: []string{"memcached", "-m=64", "-o", "modern", "-v"},
-						Ports: []corev1.ContainerPort{{
-							ContainerPort: 15672,
-							Name:          "rabbitmqui",
-						}},
-					}},
+					TerminationGracePeriodSeconds: &terminationGracePeriodSeconds,
+					Containers: []corev1.Container{
+						corev1.Container{
+							Name:  "rabbitmq",
+							Image: "rabbitmq:3.8-management",
+						},
+					},
 				},
 			},
 		},
 	}
-	// Set Memcached instance as the owner of the Deployment.
-	controllerutil.SetControllerReference(m, dep, r.Scheme)
-	return dep
+
+	controllerutil.SetControllerReference(m, statefulset, r.Scheme)
+	return statefulset, nil
+}
+
+// serviceForRabbitMQ function takes in a RabbitMQ object and returns a Service for that object.
+func (r *RabbitMQInstanceReconciler) serviceForRabbitMQ(m *susev1beta1.RabbitMQInstance) *corev1.Service {
+	ls := labelsForRabbitMQ(m.Name)
+	ser := &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      m.Name,
+			Namespace: m.Namespace,
+		},
+		Spec: corev1.ServiceSpec{
+			Selector: ls,
+			Ports: []corev1.ServicePort{
+				{
+					Port: 15672,
+					Name: m.Name,
+				},
+			},
+		},
+	}
+	// Set Memcached instance as the owner of the Service.
+	controllerutil.SetControllerReference(m, ser, r.Scheme)
+	return ser
 }
